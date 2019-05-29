3d0f9dfa37b327487257fe32f7a3f2654ee1e8e3
peterokwara <peter.okwara@outlook.com>
2019-05-29 15:18:28 +0300

    Test when button is clicked a component is created

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -19,3 +19,9 @@ it("it adds a new gift to `state` when clicking `add gift` button", () => {
   app.find(".btn-add").simulate("click");
   expect(app.state().gifts).toEqual([{ id: 1 }]);
 });
+
+it("adds a new gift to the rendered list when clicking the add gift button", () => {
+  app.find(".btn-add").simulate("click");
+
+  expect(app.find(".gift-list").children().length).toEqual(2);
+});
