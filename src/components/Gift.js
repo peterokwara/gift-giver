f141b3ac0a5364a2c846b164feca7b49f75ce1fd
peterokwara <peter.okwara@outlook.com>
2019-05-29 17:08:15 +0300

    Create the necessary state component

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Gift extends Component {
+  constructor() {
+    super();
+    this.state = { person: "", present: "" };
+  }
   render() {
     return <div />;
   }
