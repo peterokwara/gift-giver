a5a31d069ac18e99d35a3b3b510c1b13b6ab593b
peterokwara <peter.okwara@outlook.com>
2019-05-29 15:17:51 +0300

    Create new component when the button is clicked

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,14 @@ class App extends Component {
     return (
       <div>
         <h2> Gift giver</h2>
-        <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
+        <div className="gift-list">
+          {this.state.gifts.map(gift => {
+            return <div key={gift.id} />;
+          })}
+        </div>
+        <Button className="btn-add" onClick={this.addGift}>
+          Add Gift
+        </Button>
       </div>
     );
   }
